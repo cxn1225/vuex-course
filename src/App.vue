d931109d478fdc4836de929046bf9581c93475bb
cxn <675087841@qq.com>
2019-08-24 17:52:54 +0800

    lessol-4

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
     app.vue:{{getCount}}
-    <p>{{todos}}</p>
-    <p>{{completedTodos}}</p>
+
     <p>{{completedTodosCounts}}</p>
     <p>{{getTodoById(2)}}</p>
-    <button @click="decrementCount(5)">减</button>
+    <button @click="decrementCountAsync(5)">减</button>
     {{getCount}}
-    <button @click="incrementCount">加</button>
+    <button @click="incrementCountAsync">加</button>
+    <hr>
+    <p>{{completedTodos}}</p>
+    <button @click="fetacDataAsync">FetchData</button>
   </div>
 </template>
 
 <script>
-  import { mapState, mapGetters, mapMutations } from "vuex"
+  import { mapState, mapGetters, mapMutations, mapActions } from "vuex"
   import Count from "./components/Count.vue"
   export default {
     name: 'app',
-
-    /* 简写：调用vuex的getters */
     computed: mapGetters([
       "getCount",
       "completedTodos",
@@ -25,18 +25,20 @@
       "getTodoById"
     ]),
 
-    /* methods: {
-      incrementCount() {    //加
-        //调用vuex的mutations
-        this.$store.commit("incrementCount")
-      },
-      decrementCount(num) {     //减（传参）
-        this.$store.commit("decrementCount", num)
-      }
-    }, */
-
-    /* 简写 */
-    methods: mapMutations(["incrementCount", "decrementCount"])
+    /*  methods: {
+       incrementCountAsync() {
+         //触发actions
+         this.$store.dispatch("incrementCountAsync")
+       },
+       decrementCountAsync(n) {
+         //触发actions(传参)
+         this.$store.dispatch("decrementCountAsync", n)
+       },
+       fetacDataAsync() {
+         this.$store.dispatch("fetacDataAsync")
+       }
+     }, */
+    methods: mapActions(["incrementCountAsync", "decrementCountAsync", "fetacDataAsync"])
   }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -21,12 +21,30 @@ export default new Vuex.Store({
     completedTodosCounts: (state, getters) => getters.completedTodos.length,
     getTodoById: state => id => state.todos.find(todo => todo.id == id)
   },
-  /* 更改属性 */
+  /* 更改属性(同步请求) */
   mutations: {
-    incrementCount: state => state.count++,
-    decrementCount: (state, num) => state.count -= num
+    incrementCount: state => state.count++,   //加
+    decrementCount: (state, num) => state.count -= num,    //减（传参）
+    setTodos: (state, data) => state.todos = data   //修改todos
   },
+  //更改属性(异步请求)，实际上是异步调用mutations
   actions: {
-
+    incrementCountAsync: ({ commit }) => {   //加
+      setTimeout(() => {
+        //context相当于this.$store
+        commit("incrementCount")
+      }, 2000)
+    },
+    decrementCountAsync: ({ commit }, payload) => {   //减（传参）
+      setTimeout(() => {
+        commit("decrementCount", payload)
+      }, 1000)
+    },
+    //ES9完美异步：async
+    async fetacDataAsync({ commit }) {     //从网络获取todos
+      //await修饰的语句中数据请求完成后才执行下一句
+      const response = await axios.get("http://jsonplaceholder.typicode.com/todos")
+      commit("setTodos", response.data)
+    }
   }
 })
